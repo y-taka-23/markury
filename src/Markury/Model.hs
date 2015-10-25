d7f6a2e61df913ab9defedc09128e2ada91e7888
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2015-10-26 02:18:41 +0900

    Define the simplest model

---
+++ src/Markury/Model.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+module Markury.Model where
+
+import Database.Persist.TH
+import Data.Text ( Text )
+import Data.Time ( UTCTime )
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Bookmark
+    title       Text
+    description Text Maybe
+    url         Text
+    created     UTCTime
+    modified    UTCTime
+|]
