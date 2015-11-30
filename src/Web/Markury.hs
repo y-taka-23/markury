2725e215cde607b8aea8a40c145ca2d34af174f1
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2015-11-30 15:37:19 +0900

    Extract the HTML rendering function

--- src/Web/Markury.hs
+++ src/Web/Markury.hs
@@ -7,9 +7,10 @@ import Web.Markury.Model
 
 import Web.Spock.Digestive ( runForm )
 import Web.Spock.Safe
+import Text.Blaze.Html ( Html )
 import Text.Blaze.Html.Renderer.Utf8 ( renderHtml )
 import Control.Monad ( forM_ )
-import Control.Monad.IO.Class ( liftIO )
+import Control.Monad.IO.Class ( MonadIO, liftIO )
 import Control.Monad.Logger ( NoLoggingT, runNoLoggingT )
 import Control.Monad.Trans.Resource ( ResourceT, runResourceT )
 import qualified Database.Persist as P
@@ -24,18 +25,18 @@ runMarkury = do
     runSpock 8080 $ spock (defaultSpockCfg Nothing (PCPool pool) Nothing) $ do
         get "/bookmarks" $ do
             allBookmarks <- runSql $ P.selectList [] [P.Asc BookmarkCreated]
-            lazyBytes $ renderHtml $ siteView $ bookmarkListView (map P.entityVal allBookmarks) 1 1
+            renderSite $ bookmarkListView (map P.entityVal allBookmarks) 1 1
         get ("/bookmarks/view" <//> var ) $ \id -> do
             mBookmark <- runSql $ P.get $ BookmarkKey id
             case mBookmark of
-                Just bookmark -> lazyBytes $ renderHtml $ siteView $ bookmarkView (unSqlBackendKey id) bookmark
+                Just bookmark -> renderSite $ bookmarkView (unSqlBackendKey id) bookmark
                 Nothing -> redirect "/bookmarks"
         getpost "/bookmarks/add" $ do
             now <- liftIO getCurrentTime
             f <- runForm "addBookmark" $ bookmarkAddForm now now
             case f of
                 (view, Nothing) -> do
-                    lazyBytes $ renderHtml $ siteView $ bookmarkAddView view "/bookmarks/add"
+                    renderSite $ bookmarkAddView view "/bookmarks/add"
                 (_, Just newBookmark) -> do
                     bookmarkId <- runSql $ P.insert newBookmark
                     tags <- runSql $ P.selectList [TagTitle P.<-. []] []
@@ -45,38 +46,41 @@ runMarkury = do
                     redirect "/bookmarks"
         get "/users" $ do
             allUsers <- runSql $ P.selectList [] [P.Asc UserCreated]
-            lazyBytes $ renderHtml $ siteView $ userListView (map P.entityVal allUsers) 1 1
+            renderSite $ userListView (map P.entityVal allUsers) 1 1
         get ("/users/view" <//> var ) $ \id -> do
             mUser <- runSql $ P.get $ UserKey id
             case mUser of
-                Just user -> lazyBytes $ renderHtml $ siteView $ userView (unSqlBackendKey id) user
+                Just user -> renderSite $ userView (unSqlBackendKey id) user
                 Nothing -> redirect "/users"
         getpost "/users/add" $ do
             now <- liftIO getCurrentTime
             f <- runForm "addUser" $ userAddForm now now
             case f of
                 (view, Nothing) -> do
-                    lazyBytes $ renderHtml $ siteView $ userAddView view "/users/add"
+                    renderSite $ userAddView view "/users/add"
                 (_, Just newUser) -> do
                     _ <- runSql $ P.insert newUser
                     redirect "/users"
         get "/tags" $ do
             allTags <- runSql $ P.selectList [] [P.Asc TagCreated]
-            lazyBytes $ renderHtml $ siteView $ tagListView (map P.entityVal allTags) 1 1
+            renderSite $ tagListView (map P.entityVal allTags) 1 1
         get ("/tags/view" <//> var ) $ \id -> do
             mTag <- runSql $ P.get $ TagKey id
             case mTag of
-                Just tag -> lazyBytes $ renderHtml $ siteView $ tagView (unSqlBackendKey id) tag
+                Just tag -> renderSite $ tagView (unSqlBackendKey id) tag
                 Nothing -> redirect "/tags"
         getpost "/tags/add" $ do
             now <- liftIO getCurrentTime
             f <- runForm "addTag" $ tagAddForm now now
             case f of
                 (view, Nothing) -> do
-                    lazyBytes $ renderHtml $ siteView $ tagAddView view "/tags/add"
+                    renderSite $ tagAddView view "/tags/add"
                 (_, Just newTag) -> do
                     _ <- runSql $ P.insert newTag
                     redirect "/tags"
 
 runSql :: (HasSpock m, SpockConn m ~ SqlBackend) => SqlPersistT (NoLoggingT (ResourceT IO)) a -> m a
 runSql action = runQuery $ \conn -> runResourceT $ runNoLoggingT $ runSqlConn action conn
+
+renderSite :: MonadIO m => Html -> ActionT m a
+renderSite = lazyBytes . renderHtml . siteView
